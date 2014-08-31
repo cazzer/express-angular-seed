c22041b6fac7973443c8a49c999139fe34931b86
Caleb Brewer <calebthebrewer@gmail.com>
2014-08-30 19:33:16 -0500

    abstracting routes again, because we like it like that

--- server/routes.js
+++ server/routes.js
@@ -1,69 +1,14 @@
-var db = require('./db'),
-	samples = db.collection('samples');
+var utils = require('./routes/_utils');
 
 module.exports  = function(app) {
 
 	var base = '/api/1/';
 
-	app.get(base + 'samples/:id?', function(req, res) {
-		if (paramExists(req, 'id')) {
-			samples.findOne({
-				_id: db.ObjectId(req.params.id)
-			}, function(err, samples) {
-				res.send(samples);
-			});
-		} else {
-			getAll(res);
-		}
-	});
+	require('./routes/samples')(app);
 
-	app.put(base + 'samples/:id?', function(req, res) {
-		if (paramExists(req, 'id')) {
-			samples.update({
-				_id: db.ObjectId(req.params.id)
-			}, function(err, n) {
-				res.send(err || n);
-			});
-		} else {
-			res.status(400).send('Sample ID is required.');
-		}
-	});
-
-	app.post(base + 'samples', function(req, res) {
-		samples.save(req.body, function(err, sample) {
-			res.send(sample);
-		});
-	});
-
-	app.delete(base + 'samples/:id?', function(req, res) {
-		if (paramExists(req, 'id')) {
-			samples.remove({
-				_id: db.ObjectId(req.params.id)
-			}, function(err, n) {
-				res.send(err || n);
-			});
-		} else {
-			res.status(400).send('Sample ID is required.');
-		}
-	});
-
-	app.get('*', notFound);
-	app.put('*', notFound);
-	app.post('*', notFound);
-	app.delete('*', notFound);
+	app.get('*', utils.notFound);
+	app.put('*', utils.notFound);
+	app.post('*', utils.notFound);
+	app.delete('*', utils.notFound);
 
 };
-
-function getAll(res) {
-	samples.find(function(err, samples) {
-		res.send(samples);
-	});
-}
-
-function paramExists(req, param) {
-	return !!req.params && req.params[param];
-}
-
-function notFound(req, res) {
-	res.status(404).send('That thing you were looking for is not a thing.');
-}
